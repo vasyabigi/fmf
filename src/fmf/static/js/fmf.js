b2900554a90b1daecf145912a38712114b3f28ad
Edifear <edifear@gmail.com>
2011-12-11 20:15:47 +0200

    konami

--- src/fmf/static/js/fmf.js
+++ src/fmf/static/js/fmf.js
@@ -88,3 +88,53 @@ $(document).ready(function(){
 	$('#toTop').click(function() {
 		$('body,html').animate({scrollTop:0},1000);
 	});
+
+(function($) {
+
+	$.fn.konami = function(options) {
+
+		var opts = $.extend({}, $.fn.konami.defaults, options);
+		return this.each(function() {
+
+			var masterKey = [38,38,40,40,37,39,37,39,66,65];
+			var controllerCode = [];
+			$(window).keyup(function(evt) {
+
+				var code = evt.keyCode ? evt.keyCode : evt.which;
+				controllerCode.push(code);
+				if(controllerCode.length === 10) {
+
+					var bIsValid = true;
+					for(var i = 0, l = masterKey.length; i < l; i++) {
+						if(masterKey[i] !== controllerCode[i]) {
+							bIsValid = false;
+						} // end if
+					} // end for
+
+					if(bIsValid) {
+						opts.cheat();
+					} // end if
+
+					controllerCode = [];
+
+				} // end if
+
+			}); // keyup
+
+		}); // each
+
+	}; // opts
+
+	$.fn.konami.defaults = {
+		cheat: null
+	};
+
+})(jQuery);
+
+$(function() {
+	$(window).konami({
+		cheat: function() {
+			$('.page_info > div > span').html('&#1057;&#1072;&#1084;&#1080;&#1081;&#32;&#1054;&#1061;&#1059;&#1028;&#1053;&#1053;&#1048;&#1049;&#32;&#1089;&#1072;&#1081;&#1090;&#33;&#33;&#33;&#32;');
+		}
+	});
+});
