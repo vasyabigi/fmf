d9436a29b00f6711a51a232667d6d992581e6343
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2011-12-24 12:40:27 +0200

    Added changes to main page

--- src/fmf/static/js/fmf.js
+++ src/fmf/static/js/fmf.js
@@ -165,17 +165,17 @@ $(document).ready(function(){
 
 });
 
-	$(window).scroll(function() {
-		if($(this).scrollTop() > 300) {
-			$('.toTop').fadeIn();
-		} else {
-			$('.toTop').fadeOut();
-		}
-	});
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 300) {
+            $('.toTop').fadeIn();
+        } else {
+            $('.toTop').fadeOut();
+        }
+    });
 
-	$('.toTop').click(function() {
-		$('body,html').animate({scrollTop:0},1000);
-	});
+    $('.toTop').click(function() {
+        $('body,html').animate({scrollTop:0},'slow');
+    });
 
 
 (function($) {
