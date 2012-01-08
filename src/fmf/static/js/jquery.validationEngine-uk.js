af2978df65099248fe811f15d66eed909a2d58bd
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2012-01-09 01:18:19 +0200

    Added uk translation for js validation

--- src/fmf/static/js/jquery.validationEngine-uk.js
+++ src/fmf/static/js/jquery.validationEngine-uk.js
@@ -6,19 +6,19 @@
             $.validationEngineLanguage.allRules = {
                 "required": { // Add your regex rules here, you can take telephone as an example
                     "regex": "none",
-                    "alertText": "* Необходимо заполнить",
-                    "alertTextCheckboxMultiple": "* Вы должны выбрать вариант",
-                    "alertTextCheckboxe": "* Необходимо отметить"
+                    "alertText": "* Необхідно заповнити",
+                    "alertTextCheckboxMultiple": "* Ви повинні вибрати варіант",
+                    "alertTextCheckboxe": "* Необхідно відмітити"
                 },
                 "minSize": {
                     "regex": "none",
-                    "alertText": "* Минимум ",
-                    "alertText2": " символа(ов)"
+                    "alertText": "* Мінімум ",
+                    "alertText2": " символів"
                 },
                 "maxSize": {
                     "regex": "none",
                     "alertText": "* Максимум ",
-                    "alertText2": " символа(ов)"
+                    "alertText2": " символ(ів)"
                 },
 				"groupRequired": {
                     "regex": "none",
@@ -65,7 +65,7 @@
                 "email": {
                     // Shamelessly lifted from Scott Gonzalez via the Bassistance Validation plugin http://projects.scottsplayground.com/email_address_validation/
                     "regex": /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
-                    "alertText": "* Неверный формат email"
+                    "alertText": "* Невірний формат email"
                 },
                 "integer": {
                     "regex": /^[\-\+]?\d+$/,
@@ -99,27 +99,6 @@
                 "onlyLetterNumber": {
                     "regex": /^[0-9a-zA-Z\u0400-\u04FF]+$/,
                     "alertText": "* Запрещены специальные символы"
-                },
-                // --- CUSTOM RULES -- Those are specific to the demos, they can be removed or changed to your likings
-                "ajaxUserCall": {
-                    "url": "ajaxValidateFieldUser",
-                    // you may want to pass extra data on the ajax call
-                    "extraData": "name=eric",
-                    "alertText": "* Этот пользователь уже занят",
-                    "alertTextLoad": "* Проверка, подождите..."
-                },
-                "ajaxNameCall": {
-                    // remote json service location
-                    "url": "ajaxValidateFieldName",
-                    // error
-                    "alertText": "* Это имя уже занято",
-                    // if you provide an "alertTextOk", it will show as a green prompt when the field validates
-                    "alertTextOk": "* Это имя доступно",
-                    // speaks by itself
-                    "alertTextLoad": "* Проверка, подождите..."
-                },
-                "validate2fields": {
-                    "alertText": "* Пожалуйста, введите HELLO"
                 }
             };
             
