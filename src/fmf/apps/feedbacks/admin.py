75241e0a329df43bb8c965082a1a61a80eb7c35e
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2012-01-31 22:07:21 +0200

    Removed 'home' link from main menu

--- src/fmf/apps/feedbacks/admin.py
+++ src/fmf/apps/feedbacks/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
 
 from core.admin import BaseTranslationAdmin, BaseTranslationStackedInLine
 from models import Question, Feedback, FeedbackQuestion
